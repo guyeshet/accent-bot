62aa5174832f43fec8b324ffe0f667831e4ceee0
KateZi <zkate2006@yandex.ru>
2019-07-29 13:58:24 +0300

    added description to the bot /new_word - sends a random new word and an audio sample of it /get_word <word> - sends the word and the sample audio of it when the user sends a voice message, we store it "../audio/" where name is constructed as follows "<word>_<attempt>.oga" (oga is the format)
    TODO:
    give a response after "black box" processing.

--- src/accent_bot/accent-bot.py
+++ src/accent_bot/accent-bot.py
@@ -1,3 +1,5 @@
+import random
+
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import Voice
 import requests
@@ -5,23 +7,26 @@ import re
 import logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+                    level=logging.INFO)
 
 
 class AccentBot:
 
     def __init__(self):
-        self.counter = 0
+        self.dictionary = {'abc': 0, '1,2,3' : 0, 'xyz' : 0}
+        self.audio_samples = ['abc.oga', '1,2,3.oga', 'xyz.oga']
+        self.word = None
 
-    def get_url(self):
-        contents = requests.get('https://random.dog/woof.json').json()
-        url = contents['url']
-        return url
+    # def get_url(self):
+    #     contents = requests.get('https://random.dog/woof.json').json()
+    #     url = contents['url']
+    #     return url
 
-    def bop(self, bot, update):
-        url = self.get_url()
-        chat_id = update.message.chat_id
-        bot.send_photo(chat_id=chat_id, photo=url)
+    # used for checking the run of the bot
+
+    # def set_dictionary(self, accent):
+    # set the dictionaries based on the urls?
+
 
     def echo(self, bot, update):
         text = update.message.text
@@ -29,9 +34,35 @@ class AccentBot:
         bot.send_message(chat_id=chat_id, text=text)
 
     def store(self, bot, update):
-        voice = update.message.voice.get_file()
-        self.counter += 1
-        voice.download("/Users/ekaterinazinkovskaia/PycharmProjects/accent-bot/src/audio/" + (str)(self.counter))
+        if(self.word is not None):
+            voice = update.message.voice.get_file()
+            # needs a new name to be stored by
+            word_attempt = self.word + '_' + (str)(self.dictionary[self.word])
+            self.dictionary[self.word] += 1
+            voice.download("../audio/" + word_attempt + ".oga")
+            chat_id = update.message.chat_id
+            text = "Waiting for the response"
+            bot.send_message(chat_id=chat_id, text=text)
+
+    def get_word(self):
+        # rand_item = self.dictionary[random.randrange(len(self.dictionary))]
+        rand_word, attempt = random.choice(list(self.dictionary.items()))
+        return rand_word
+
+    def get_audio(self):
+        audio = "../audio/" + self.word + ".oga"
+        return audio
+
+    def getWord(self, bot, update, args=None):
+        if args is None:
+            word = self.get_word()
+        else:
+            word = args[0]
+        self.word = word
+        audio = self.get_audio()
+        chat_id = update.message.chat_id
+        bot.send_message(chat_id=chat_id, text=self.word)
+        bot.send_voice(chat_id=chat_id, voice=open(audio, 'rb'))
 
 
 def main():
@@ -39,13 +70,19 @@ def main():
     dp = updater.dispatcher
     accent_bot = AccentBot()
 
-    dp.add_handler(CommandHandler('bop', accent_bot.bop))
+    dp.add_handler(CommandHandler('new_word', accent_bot.getWord, pass_args=False))
+    dp.add_handler(CommandHandler('get_word', accent_bot.getWord, pass_args=True))
 
     echo_handler = MessageHandler(Filters.text, accent_bot.echo)
     dp.add_handler(echo_handler)
 
     audio_handler = MessageHandler(Filters.voice, accent_bot.store)
     dp.add_handler(audio_handler)
+
+    # once the user sends the voice,
+    # we send it to the "black box"
+    # once the black box gives us result we ping it to the user
+    # Ask Guy
 
     updater.start_polling()
     updater.idle()
